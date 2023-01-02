6297f1bb8428ad2560c82ab57eed3f624ac5cbce
Zero <iamvamsi999@gmail.com>
2023-01-02 11:07:32 +0530

    any type

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -3,4 +3,10 @@ let sales = 123_456_789;
 let courses = 'Typescript';
 let is_published: boolean = true;
 let level;
+// level = 1;
+// level = 'a';
 
+function render(document: any) {
+  console.log(document);
+  
+}
