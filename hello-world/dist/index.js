a2fed4168c454f10b7caa069495016194ed604df
Zero <iamvamsi999@gmail.com>
2023-01-02 11:17:46 +0530

    tsc

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,7 +1,5 @@
 "use strict";
-let age = 50;
-if (age < 30) {
-    age + 10;
-}
-console.log(age);
+let user = [1, "zero"];
+// let user1: [number, string] = [1, "zero", 0];  second index is not initialised
+let user2 = [1, 'zero', true, 1]; // its better to tuple when key value/ 2 variables only so it is easy to understand
 //# sourceMappingURL=index.js.map
