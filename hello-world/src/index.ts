5d7d8d7d6f5a9a4629f2d54fdb63737d4d2548b3
Zero <iamvamsi999@gmail.com>
2023-01-03 16:59:37 +0530

    decorators

--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -1,6 +1,26 @@
-let age: number = 50;
-if(age < 30){
-  age+10;
-}
-console.log(age);
 
+// class decorators
+// function Component(constructor: Function) {
+//   // Here we have a chance to modify members of 
+//   // the target class
+//   constructor.prototype.uniqueId = Date.now();
+//   }
+   
+//   @Component
+//   class ProfileComponent { 
+
+//   }
+
+//Parameterized decorators
+  function Component(value: number) { return (constructor: Function) => {
+    // Here we have a chance to modify members of
+     // the target class. 
+     constructor.prototype.uniqueId = Date.now();
+    }; 
+  }
+  @Component(1)
+  class ProfileComponent {
+
+  }
+
+    
