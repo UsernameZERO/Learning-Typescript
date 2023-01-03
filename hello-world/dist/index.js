5afcb7c82219b570f871a6a2e33872dee3c19151
Zero <iamvamsi999@gmail.com>
2023-01-03 17:06:54 +0530

    decoration composition

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -1,7 +1,40 @@
 "use strict";
-let age = 50;
-if (age < 30) {
-    age + 10;
+// class decorators
+// function Component(constructor: Function) {
+//   // Here we have a chance to modify members of 
+//   // the target class
+//   constructor.prototype.uniqueId = Date.now();
+//   }
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+//Parameterized decorators
+function Component(options) {
+    return (constructor) => {
+        // Here we have a chance to modify members of
+        // the target class. 
+        console.log("component decorator called");
+        constructor.prototype.options = options;
+        constructor.prototype.uniqueId = Date.now();
+    };
 }
-console.log(age);
+// @Component(1)
+// class ProfileComponent {
+// }
+//Decorator composition
+// Multiple decorators are applied in reverse order.
+// Pipe followed by Component.
+function Pipe(constructor) {
+    console.log('Pipe decorator called');
+    constructor.prototype.pipe = true;
+}
+let ProfileComponent = class ProfileComponent {
+};
+ProfileComponent = __decorate([
+    Component({ selector: '#my-profile' }),
+    Pipe
+], ProfileComponent);
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -11,16 +11,36 @@
 
 //   }
 
-//Parameterized decorators
-  function Component(value: number) { return (constructor: Function) => {
+type ComponentOptions = {
+  selector: string
+}
+
+  //Parameterized decorators
+  function Component(options: ComponentOptions) { return (constructor: Function) => {
     // Here we have a chance to modify members of
      // the target class. 
+     console.log("component decorator called");
+     constructor.prototype.options = options;
      constructor.prototype.uniqueId = Date.now();
     }; 
   }
-  @Component(1)
+  // @Component(1)
+  // class ProfileComponent {
+
+  // }
+
+  //Decorator composition
+
+  // Multiple decorators are applied in reverse order.
+  // Pipe followed by Component.
+   function Pipe(constructor: Function){
+    console.log('Pipe decorator called');
+    constructor.prototype.pipe = true;
+    
+   }
+  @Component({ selector: '#my-profile'})
+  @Pipe
   class ProfileComponent {
 
   }
-
     
